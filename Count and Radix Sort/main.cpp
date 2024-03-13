7e49fb243aec5a2a57eb3f432233cdf39fa06b34
Erkebulan05 <shopangali05@gmail.com>
2024-03-13 13:46:33 +0500

    Quick and Merge Sort

--- Count and Radix Sort/main.cpp
+++ Count and Radix Sort/main.cpp
@@ -2,47 +2,59 @@
 #include <algorithm>
 #include <iostream>
 using namespace std;
+/// TC: O(n+k) n and k size of inputarray nad outputarrray
+//// SC:O(n+k) n and k taken space of inputarray and outputarray
+
 
 class CountSort {
-private:
     vector <int> inputArray;
     vector <int> implementation() {
         int N = inputArray.size();
-        int M = inputArray[0];
-        for(int i = 0 ; i<inputArray.size();i++)
-            if(inputArray[i]>M)
+        int M = inputArray[0]; /// max element
+        for (int i = 1; i < inputArray.size(); i++)
+            if (inputArray[i] > M)
                 M = inputArray[i];
 
-//        int M = *max_element(inputArray.begin(),inputArray.end());
-        vector <int> countArray(M+1,0);
+        vector<int> countArray(M + 1, 0);
 
-        for(int i = 0 ; i<N ; i++)
+        for(int i = 0 ; i < N ; i ++)
             countArray[inputArray[i]]++;
 
-        for(int i = 1 ; i<= M ; i++)
+        for(int i = 1 ; i <= M ; i++)
             countArray[i] += countArray[i-1];
+
 
         vector <int> outputArray(N);
 
-
-        for(int i = N-1 ; i>=0 ; i--){
-            outputArray[countArray[inputArray[i]]-1] = inputArray[i];
+        for(int i = N- 1 ;i>=0 ; i--){
+            outputArray[countArray[inputArray[i]]-1]=inputArray[i];
             countArray[inputArray[i]]--;
         }
 
         return outputArray;
 
-    }
 
+    }
+//    4, 3, 7, 1, 5, 5, 3, 9
+//    0  1   2  3  4  5  6  7  8  9
+//    0  0   0  0  0  0  0  0  0  0
+//    1  0   0 2  1  2   0  1  0 1
+//           0  1  2  3  4  5  6  7
+//    input  4, 3, 7, 1, 5, 5, 3, 9
+//          0  1  2  3  4  5  6  7  8  9
+//   count  1  1  1  3  4  6  6  7  7  8
+//         0   1   2  3  4  5  6  7  8 9
+//   outp  1    3   3  4   4  5   7  9
+//   output 1 3 3 4 4 5 7 9
 public:
     CountSort(const vector<int>& inputArray) : inputArray(inputArray) {}
     vector<int> sort() {
         return implementation();
     }
 };
-
+////TC:O(d * (n + k)), где d - количество разрядов, n - количество элементов в массиве, а k - диапазон значений элементов.
+/// SP:O(n+k)
 class RadixSort {
-private:
     // A utility function to get the maximum value in arr[]
     int getMax(int arr[], int n) {
         int mx = arr[0];
@@ -53,42 +65,54 @@ private:
     }
 
     // A function to do counting sort of arr[] according to the digit represented by exp
+
     void countSort(int arr[], int n, int exp) {
-        int output[n]; // Output array
-        int i, count[10] = { 0 };
+      int count[10] = {0};
+      int output[n];
 
-        // Store count of occurrences in count[]
-        for (i = 0; i < n; i++)
-            count[(arr[i] / exp) % 10]++;
+      for(int i = 0 ; i <n ;i++)
+          count[(arr[i]/exp)%10]++;
 
-        // Change count[i] so that count[i] now contains actual position of this digit in output[]
-        for (i = 1; i < 10; i++)
-            count[i] += count[i - 1];
+      for(int i = 1 ; i < 10 ; i++)
+          count[i] +=count[i-1];
 
-        // Build the output array
-        for (i = n - 1; i >= 0; i--) {
-            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
-            count[(arr[i] / exp) % 10]--;
-        }
-
-        // Copy the output array to arr[], so that arr[] now contains sorted numbers according to the current digit
-        for (i = 0; i < n; i++)
-            arr[i] = output[i];
+      for(int i =n-1 ; i>=0;i--){
+          output[count[(arr[i]/exp)%10]-1] = arr[i];
+          count[(arr[i]/exp)%10]--;
+      }
+      for(int i = 0 ; i < n ; i++)
+          arr[i] = output[i];
     }
 
 public:
-    // The main function that sorts arr[] of size n using Radix Sort
     void radixsort(int arr[], int n) {
         // Find the maximum number to know the number of digits
         int m = getMax(arr, n);
-
         // Do counting sort for every digit. Note that instead of passing digit number, exp is passed. exp is 10^i where i is the current digit number
         for (int exp = 1; m / exp > 0; exp *= 10)
             countSort(arr, n, exp);
     }
 };
-
-
+//986 329 217 543 765
+//986
+//exp = 1 986/1>0; exp*=10
+//        countsort;
+//output[5]
+//int i
+//count   0   1     2   3     4   5     6   7   8   9
+//        0   0     0   0     0   0     0   0   0   0
+//        0   0     0   1     0   1     0   1   1   1
+////                      543       765      217 986  329;
+//        0   0     0   1     1   2     2    3  4   5
+//
+//        0   1   2   3   4   5  6   7   8   9
+//        543  765  217  986  329
+//        0  1  2  3  4   5  6  7  8  9
+//           217   329      543  765   986
+//
+//           217 329 543 765 986
+//
+//
 
 
 
@@ -118,4 +142,3 @@ int main() {
 
     return 0;
 }
-
